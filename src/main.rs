b7a385d05a7839706645b7c747cba39fd5904e9c
randijunior <randi.junior@55pbx.com.br>
2026-08-14 12:19:57 -0300

    update rssip version

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,114 @@
 use clap::Parser;
-use uuid::Uuid;
-use voip_sip::{
-    Endpoint,
-    message::{Request, headers, method, sip_uri},
-    transaction::TsxPlugin,
+use rssip::{
+    Endpoint, IncomingResponse, OutgoingRequest,
+    endpoint::{self, ToTake},
+    message::{Request, headers, method, uri},
+    transaction::{ClientTransaction, TsxPlugin},
 };
+use tracing::Level;
+use tracing_subscriber::fmt::time::ChronoLocal;
+use uuid::Uuid;
 
 use crate::cli::Cli;
 
 mod cli;
 
+struct Logger;
+
+#[async_trait::async_trait]
+impl endpoint::Plugin for Logger {
+    fn name(&self) -> &'static str {
+        "logger"
+    }
+
+    async fn on_outgoing_request(&self, req: &mut OutgoingRequest) {
+        println!("{}{}", req.req_line, req.headers);
+    }
+    async fn on_incoming_response(&self, res: ToTake<'_, IncomingResponse>, _endpoint: &Endpoint) {
+        println!("{}{}", res.status_line, res.headers);
+    }
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::parse();
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .with_env_filter("rssip=trace")
+        .with_timer(ChronoLocal::new(String::from("%H:%M:%S%.3f")))
+        .init();
 
+    let args = Cli::parse();
     let target = args.uri.parse()?;
 
-    let endpoint = Endpoint::builder()
-        .with_plugin(TsxPlugin::default())
-        .with_udp_addr("0.0.0.0:9080")
-        .build()
-        .await?;
+    let endpoint = create_endpoint().await?;
+    let options_request = build_options_request(target);
 
-    let request = create_options_request(target);
+    let client_tsx = ClientTransaction::send_request(options_request, endpoint).await?;
 
-    let client_tsx = endpoint.send_outgoing_request(request).await?;
-
-    let _response = client_tsx.receive_final_response().await?;
+    let _ = client_tsx.receive_final_response().await?;
 
     Ok(())
 }
 
-fn create_options_request(target: sip_uri::Uri) -> Request {
+async fn create_endpoint() -> rssip::Result<Endpoint> {
+    Endpoint::builder()
+        .with_plugin(Logger)
+        .with_plugin(TsxPlugin::default())
+        .with_udp_addr("0.0.0.0:9080")
+        .build()
+        .await
+}
+
+fn build_options_request(target: uri::Uri) -> Request {
     let headers = build_headers(&target);
-    let request = Request::with_headers(method::Method::Options, target, headers);
+    let request = Request::with_headers(method::SipMethod::Options, target, headers);
     request
 }
 
-fn build_headers(target: &sip_uri::Uri) -> headers::Headers {
-    let hostip = voip_sip::utils::local_ip::get_local_ip_addr();
+fn build_headers(target: &uri::Uri) -> headers::Headers {
+    use headers::Header;
+    let hostip = rssip::utils::local_ip::get_local_ip_addr();
 
-    let host = sip_uri::Host::IpAddr(hostip);
-    let host_port = sip_uri::HostPort {
+    let host = uri::Host::IpAddr(hostip);
+
+    let host_port = uri::HostPort {
         host: host.clone(),
         port: Some(9080),
     };
 
-    let uri = sip_uri::UriBuilder::new()
-        .user(sip_uri::UserInfo {
+    let uri = uri::Uri {
+        scheme: target.scheme,
+        user: Some(uri::UserInfo {
             user: "sipc".to_owned(),
             pass: None,
-        })
-        .host(host_port.clone())
-        .scheme(target.scheme)
-        .build();
+        }),
+        host_port: host_port.clone(),
+        ..Default::default()
+    };
+    let uri = uri::SipUri::NameAddr(uri::NameAddr { display: None, uri });
 
-    let uri = sip_uri::NameAddr::new(uri);
-    let uri = sip_uri::SipUri::NameAddr(uri);
-
-    let via = headers::Via::new_udp(host_port.clone(), None, Some(headers::via::Rport(None)));
+    let via = headers::Via::new_udp(host_port, None, Some(headers::via::Rport(None)));
     let from = headers::From {
-        uri: uri.clone(),
-        tag: None,
+        uri: uri,
+        tag: Some(rssip::utils::generate_tag()),
         params: Default::default(),
     };
     let to = headers::To {
-        uri: sip_uri::SipUri::Uri(target.clone()),
+        uri: uri::SipUri::Uri(target.clone()),
         tag: None,
         params: Default::default(),
     };
-    let cseq = headers::CSeq::new(1, method::Method::Options);
+    let cseq = headers::CSeq::new(1, method::SipMethod::Options);
     let call_id = headers::CallId::new(format!("{}@{}", Uuid::new_v4(), host));
     let max_forwards = headers::MaxForwards::new(70);
-    let contact: headers::Contact = headers::Contact {
-        uri,
-        q: None,
-        expires: None,
-        param: Default::default(),
-    };
 
-    let headers = voip_sip::headers! {
-        headers::Header::Via(via),
-        headers::Header::From(from),
-        headers::Header::To(to),
-        headers::Header::CallId(call_id),
-        headers::Header::Contact(contact),
-        headers::Header::CSeq(cseq),
-        headers::Header::MaxForwards(max_forwards)
+    let headers = rssip::headers! {
+        Header::Via(via),
+        Header::From(from),
+        Header::To(to),
+        Header::CallId(call_id),
+        Header::CSeq(cseq),
+        Header::MaxForwards(max_forwards),
     };
 
     headers
